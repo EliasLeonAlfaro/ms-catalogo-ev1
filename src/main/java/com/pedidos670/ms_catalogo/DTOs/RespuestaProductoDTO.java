2da7757f9ff32404037466c4fde1b9de777239d2
Elias Casa <eliasaaronalfaro@gmail.com>
2026-09-22 14:48:01 -0300

    creacion DTOs: Creacion y respuesta

--- src/main/java/com/pedidos670/ms_catalogo/DTOs/RespuestaProductoDTO.java
+++ src/main/java/com/pedidos670/ms_catalogo/DTOs/RespuestaProductoDTO.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @Builder
 public class RespuestaProductoDTO {
-    //objeto expuesto para no devolevr la entidad JPA
+    //objeto expuesto para no devolevr la entidad JPAasd
     private String name;
     private String description;
     private BigDecimal price;
