2fcc76042f46bcebb251a6389926f3acd281086c
Elias Casa <eliasaaronalfaro@gmail.com>
2026-09-22 15:44:01 -0300

    creacion de CatalogoServicio

--- src/main/java/com/pedidos670/ms_catalogo/Servicios/CatalogoServicio.java
+++ src/main/java/com/pedidos670/ms_catalogo/Servicios/CatalogoServicio.java
@@ -37,5 +37,56 @@ public class CatalogoServicio {
         }
         return resultado;
     }
+    //obtener un videojuego especifico mediante su ID
+    public RespuestaProductoDTO obtenerPorId(Long id){
+        Producto juego = productoRepositorio.findById(id).orElseThrow(()-> new ProductoNoEncontradoExcepcion("El juego no fue encontrado"))
+                return convertirDto(juego);
+    }
 
+    //guardar un nuevo juego en BD
+    public RespuestaProductoDTO crearJuego(GuardarProductoDTO dto){
+        Producto nuevoJuego = Producto.builder()
+                .nombre(dto.getNombre())
+                .descripcion(dto.getDescripcion())
+                .precio(dto.getPrecio())
+                .stock(dto.getStock())
+                .formato(dto.getFormato())
+                .build();
+        Producto juegoGuardado = productoRepositorio.save(nuevoJuego);
+        return convertirDto(juegoGuardado);
+    }
+    //moficiar producto existente
+    public RespuestaProductoDTO actualizarJuego(Long id, GuardarProductoDTO dto){
+        Producto juegoExistente = productoRepositorio.findById(id).orElseThrow(()-> new ProductoNoEncontradoExcepcion("No se puede un producto inexistente"));
+        juegoExistente.setNombre(dto.getNombre());
+        juegoExistente.setDescripcion(dto.getDescripcion());
+        juegoExistente.setPrecio(dto.getPrecio());
+        juegoExistente.setStock(dto.getStock());
+        juegoExistente.setFormato(dto.getFormato());
+
+        return convertirDto(productoRepositorio.save(juegoExistente));
+    }
+
+    //descuento de inv por compra realizada
+    public RespuestaProductoDTO descontarStock(Long id, Integer cantidadAComprar){
+        Producto juego = productoRepositorio.findById(id).orElseThrow(()-> new ProductoNoEncontradoExcepcion("Juego no encontrado"));
+        // validar si hay inventario dispo
+        if (juego.getStock() < cantidadAComprar){
+            throw new SinStockExcepcion("Stock insuficiente. A comprar:" + cantidadAComprar +", Disponible:" + juego.getStock());
+        }
+
+        //restar de inventario y guardar
+        juego.setStock(juego.getStock() - cantidadAComprar);
+        return convertirDto(productoRepositorio.save(juego));
+    }
+
+    //eliminar producto
+    public void eliminarJuego(Long id){
+        if(!productoRepositorio.existsById(id)){
+            throw new ProductoNoEncontradoExcepcion("No se puede eliminar un producto inexistente");
+
+        }
+        productoRepositorio.deleteById(id);
+
+    }
 }
