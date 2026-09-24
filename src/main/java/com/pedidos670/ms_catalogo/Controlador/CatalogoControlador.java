c5af3c9e5fe699bc955c0ae9de0a3aa3aa9d1b15
jfuenzalidaleal <joaquin.fuenzalida14@gmail.com>
2026-09-24 11:28:39 -0400

    lol

--- src/main/java/com/pedidos670/ms_catalogo/Controlador/CatalogoControlador.java
+++ src/main/java/com/pedidos670/ms_catalogo/Controlador/CatalogoControlador.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/api/catalogo")
+@CrossOrigin(origins="*")
 @RequiredArgsConstructor
 public class CatalogoControlador {
     private final CatalogoServicio catalogoServicio;
